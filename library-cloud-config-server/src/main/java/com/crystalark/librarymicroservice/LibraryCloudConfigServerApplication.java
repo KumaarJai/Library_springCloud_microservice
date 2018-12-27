43ddfbb9fd215975014dfbfaf6d6b6fed190556b
Ajay Rabidas <Ajay_Rabidas@epam.com>
2018-12-27 16:49:30 +0530

    added spring-cloud-config-server to project

---
+++ library-cloud-config-server/src/main/java/com/crystalark/librarymicroservice/LibraryCloudConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.crystalark.librarymicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryCloudConfigServerApplication.class, args);
+	}
+
+}
+
